d43587e5ead13e0fc4631da4845a8fe5d122114c
Rich Daley <rich@fishpercolator.co.uk>
2017-07-13 16:30:41 +0100

    Update to use nodejs instead of therubyracer

--- docker-rails-new.rb
+++ docker-rails-new.rb
@@ -26,6 +26,7 @@ system 'rails', 'new', '--database=postgresql', '--skip-bundle', appname, *args
 {
   'docker-compose.yml' => '/',
   'Dockerfile'         => '/',
+  'package.json'       => '/',
   'puma.rb'            => '/config',
   'database.yml'       => '/config'
 }.each do |file, path|
@@ -36,8 +37,7 @@ end
 
 # Make sure the gems we need are in the Gemfile
 gemfile = File.read("#{appname}/Gemfile")
-gemfile.sub!(/\n\n\n/, "\n\nruby '2.4.0'\n\n")
-gemfile.sub!(/# gem 'therubyracer'/, "gem 'therubyracer'")
+gemfile.sub!(/\n\n\n/, "\n\nruby '2.4.1'\n\n")
 
 gemfile += "\ngroup :production do\n  gem 'rails_12factor'\nend\n"
 
